0aedb7187c7495929273dec929d040841c4e88ac
Agung Setyo Pribadi <agungsetyopribadi8@gmail.com>
2021-06-18 15:18:38 +0700

    Set mqtt:publish at console command

--- routes/console.php
+++ routes/console.php
@@ -23,3 +23,9 @@ Artisan::command('mqtt:subscribe {topic} {--qos=0}', function ($topic, $qos) {
     $this->info("Connecting to MQTT broker server with topic '$topic'....");
     MqttHelper::subscribe($topic, $qos);
 });
+
+Artisan::command('mqtt:publish {topic} {--qos=0} {--retain=false}', function ($topic, $qos, $retain) {
+    $msg = $this->ask('The message is');
+    $this->info("Connecting to MQTT broker server with topic '$topic'....");
+    MqttHelper::publish($topic, $msg, $qos, $retain);
+});
